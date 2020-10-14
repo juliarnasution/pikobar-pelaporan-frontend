33bf74bba2c9cc5dd606c353087a114d77820aa9
Arif <arifboyz16@gmail.com>
2020-10-14 14:34:01 +0700

    implement purge css

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,23 @@
 const path = require('path')
 const pkg = require('./package.json')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const glob = require('glob')
+const glob = require('glob-all')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const PurgeCSS = require('purgecss/lib')
+const PurgecssPlugin = require('purgecss-webpack-plugin')
 
 let plugins = [
   new MiniCssExtractPlugin({
     filename: "[name].css",
   }),
-  new Purgecss().purge({
-    content: ["**/*.html"],
-    css: ["**/*.css"],
+  new PurgecssPlugin({
+      paths: glob.sync([
+          path.join(__dirname, './src/index.html'),
+          path.join(__dirname, './src/**/*.vue'),
+          path.join(__dirname, './src/**/*.css'),
+          path.join(__dirname, './src/**/*.js')
+      ]),
+      minify: true
   }),
   new webpack.IgnorePlugin(
     /^\.\/locale$/, /moment$/
