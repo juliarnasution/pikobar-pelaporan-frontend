e4fd05a78986696d06374e4e85120c48687f234b
Arif <arifboyz16@gmail.com>
2020-11-05 17:22:43 +0700

    change lang momentjs to lacal id

--- src/store/index.js
+++ src/store/index.js
@@ -19,20 +19,6 @@ import publicPlace from './modules/publicPlace'
 
 Vue.use(Vuex)
 
-// https://webpack.js.org/guides/dependency-management/#requirecontext
-// const modulesFiles = require.context('./modules', true, /index.js$/)
-
-// // you do not need `import app from './modules/app'`
-// // it will auto require all vuex module from modules file
-// const modules = modulesFiles.keys().reduce((modules, modulePath) => {
-//   // set './app.js' => 'app'
-//   const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
-//   const value = modulesFiles(modulePath)
-//   console.log(value.default)
-//   modules[moduleName] = value.default
-//   return value.default
-// }, {})
-
 const store = new Vuex.Store({
   modules: {
     permission,
