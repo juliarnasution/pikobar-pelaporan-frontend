ada713625d9637a7bf9b77d0398909d27d282fdf
Arif <arifboyz16@gmail.com>
2020-12-03 14:24:18 +0700

    change label recap form Kecamatan/Kelurahan to Kecamatan

--- src/components/DashboardCaseNew/Table/RecapStatus.vue
+++ src/components/DashboardCaseNew/Table/RecapStatus.vue
@@ -114,7 +114,7 @@ export default {
     }
   },
   mounted() {
-    const nameDistrict = rolesWidget['superadmin'].includes(this.roles[0]) ? 'KOTA/KAB' : 'Kecamatan/Kelurahan'
+    const nameDistrict = rolesWidget['superadmin'].includes(this.roles[0]) ? 'KOTA/KAB' : 'Kecamatan'
     this.headersStatus.unshift(
       { text: '#', value: '_id', sortable: false },
       { text: nameDistrict, value: '_id' }
