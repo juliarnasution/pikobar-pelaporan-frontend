258e192eb0a264851603a9ee838feb6e20f3f18b
Arif <arifboyz16@gmail.com>
2020-12-22 16:02:06 +0700

    notification list on click detail verification case

--- src/components/Notification/NotificationList.vue
+++ src/components/Notification/NotificationList.vue
@@ -1,40 +1,64 @@
 <template>
-  <v-navigation-drawer
-    v-model="isShowDrawer"
-    class="mt-16"
-    width="320"
-    right
-    fixed
-    temporary
-    overflow
-  >
-    <v-list>
-      <v-list-item class="font-weight-bold" @click="onClose">
-        <v-icon>mdi-chevron-left</v-icon>
-        {{ $t('label.notification') }}
-      </v-list-item>
-    </v-list>
-    <v-skeleton-loader
-      :loading="isLoading"
-      type="list-item-three-line"
+  <div>
+    <v-navigation-drawer
+      v-model="isShowDrawer"
+      class="mt-16"
+      width="320"
+      right
+      fixed
+      temporary
+      overflow
     >
-      <v-list d-flex dense style="max-height: 72vh;overflow: auto;">
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :style="item.isRead ? '':'background: #C0EDFF 30%;'"
-          link
-        >
-          <notification-item :item="item" :on-read="onRead" />
+      <v-list>
+        <v-list-item class="font-weight-bold" @click="onClose">
+          <v-icon>mdi-chevron-left</v-icon>
+          {{ $t('label.notification') }}
         </v-list-item>
       </v-list>
-    </v-skeleton-loader>
-    <v-list>
-      <v-list-item class="justify-center">
-        <span style="color: #27AE60;">{{ $t('label.see_all_notifications') }}</span>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
+      <v-skeleton-loader
+        :loading="isLoading"
+        type="list-item-three-line"
+      >
+        <v-list d-flex dense style="max-height: 72vh;overflow: auto;">
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            :style="item.isRead ? '':'background: #C0EDFF 30%;'"
+            link
+          >
+            <notification-item :item="item" :on-read="onRead" />
+          </v-list-item>
+        </v-list>
+      </v-skeleton-loader>
+      <v-list>
+        <v-list-item class="justify-center">
+          <span style="color: #27AE60;">{{ $t('label.see_all_notifications') }}</span>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <verification-form
+      :show-form="showVerificationForm"
+      :show.sync="showVerificationForm"
+      :show-confirmation.sync="showConfirmation"
+      :case-data="caseDetail"
+      :close-contact-case="closeContactCase"
+      :query-data="verificationQuery"
+      :query.sync="verificationQuery"
+    />
+    <verification-confirmation
+      :show-dialog="showConfirmation"
+      :show.sync="showConfirmation"
+      :query-data="verificationQuery"
+      :query.sync="verificationQuery"
+      :submit-data.sync="isSubmit"
+    />
+    <dialog-failed
+      :show-dialog="showFailedDialog"
+      :show.sync="showFailedDialog"
+      :title="$t('label.verification_expired_title')"
+      :message="$t('label.verification_expired_message')"
+    />
+  </div>
 </template>
 <script>
 import { mapGetters } from 'vuex'
@@ -51,7 +75,20 @@ export default {
     return {
       params: {},
       items: [],
-      isLoading: false
+      isLoading: false,
+      showVerificationForm: false,
+      closeContactCase: [],
+      showConfirmation: false,
+      caseDetail: null,
+      showFailedDialog: false,
+      isSubmit: false,
+      verificationQuery: {
+        'id': '',
+        'data': {
+          'verified_status': '',
+          'verified_comment': ''
+        }
+      }
     }
   },
   computed: {
@@ -76,9 +113,11 @@ export default {
   },
   methods: {
     async onRead(item) {
-      await this.$store.dispatch('notifications/onReadNotification', item._id)
-      item.isRead = true
-      // caseID
+      if (!item.isRead) {
+        await this.$store.dispatch('notifications/onReadNotification', item._id)
+        item.isRead = true
+      }
+      this.handleDetail(item.referenceId)
     },
     onClose() {
       this.$store.dispatch('notifDrawer/notificationDrawer', !this.isShowDrawer)
@@ -88,6 +127,17 @@ export default {
       const response = await this.$store.dispatch('notifications/getListNotifications', this.params)
       this.items = response.data ? response.data.itemsList : []
       this.isLoading = false
+    },
+    async handleDetail(id) {
+      const response = await this.$store.dispatch('reports/detailReportCase', id)
+      const responseCloseContact = await this.$store.dispatch('closeContactCase/getListCloseContactByCase', id)
+      if (response.data.verified_status === 'verified') {
+        this.showFailedDialog = true
+      } else {
+        this.caseDetail = response.data
+        this.closeContactDetail = responseCloseContact.data
+        this.showVerificationForm = true
+      }
     }
   }
 }
