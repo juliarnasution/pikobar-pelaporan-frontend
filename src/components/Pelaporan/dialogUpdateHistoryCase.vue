54da516e0d69550e54f635ab3154b0523a6e7391
Arif <arifboyz16@gmail.com>
2021-01-08 11:46:04 +0700

    improvement layout update history case

--- src/components/Pelaporan/dialogUpdateHistoryCase.vue
+++ src/components/Pelaporan/dialogUpdateHistoryCase.vue
@@ -8,7 +8,7 @@
           <v-icon @click="show = false">mdi-close</v-icon>
         </v-card-title>
         <v-divider />
-        <v-container>
+        <v-row class="mx-6">
           <ValidationObserver ref="observer">
             <v-form
               ref="form"
@@ -60,7 +60,7 @@
               </v-container>
             </v-form>
           </ValidationObserver>
-        </v-container>
+        </v-row>
       </v-card>
     </v-dialog>
   </v-row>
